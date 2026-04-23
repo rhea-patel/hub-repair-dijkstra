7d8dbe947229cfc641389fc396e12aa2398a7124
Riya Patel <63653782+rhea-patel@users.noreply.github.com>
2026-04-23 11:48:14 -0400

    Implement Dijkstra shortest-path logic in PowerService

--- src/DamagedPostalCodes.java
+++ src/DamagedPostalCodes.java
@@ -1,55 +1,7 @@
 package ps.csci3901;
-import java.sql.DriverManager;
-import java.util.List;
-import java.sql.Connection;
-import java.sql.Statement;
-import java.sql.ResultSet;
-import java.util.ArrayList;
 
-public class DamagedPostalCodes extends PowerService {
-
-    private Connection c;
-    private List<DamagedPostalCodes> Postalcodes;
-
-    public List<DamagedPostalCodes> mostDamagedPostalCodes (int limit){
-        List<DamagedPostalCodes> list = new ArrayList<> ();
-
-        try {
-           Statement statement = c.createStatement();
-            // Executing the SQL Query
-           String s = "SELECT Postalcode.Postalcodes, hubdamage.repairTime\\r\n" +
-                   "\"FROM DistributionHub\\n\" +\n" +
-                   "\"JOIN hubdamage ON DistributionHub.hubIdentifier = hubdamage.damagedhub\\n\" +\n" +
-                   "\"JOIN Postalcode ON DistributionHub.Postalcodes = Postalcode.Postalcodes\\n\"\n" +
-                   "\"'ORDER BY hubdamage.repairTime DESC Limit\" *limits\" ;ln) ;";
-
-            ResultSet rs = statement.executeQuery(s);
-
-           while (rs.next()) {
-               Postalcodes.get ( rs.getInt ( "limit" ) );
-           }
-            // close the sql connection
-           rs.close();
-       } catch (Exception e) {
-           System.out.println(e);
-       }
-        return Postalcodes;
-   }
-    public static void main( String[] args){
-        try{
-            // Connecting to Dal Database
-
-            Class.forName( "com.mysql.cj.jdbc.Driver" );
-            String dbURL="jdbc:mysql://db.cs.dal.ca:3306/jdbcdb";
-            String CSID="riyap";
-            String BANNER_ID="B00930901";
-            connection = DriverManager.getConnection("jdbc:mysql://db.cs.dal.ca:3306/csci3901/riyap")("dbURL","CSID","BANNER_ID ");
-            statement.execute( "use riyap;" );
-        }
-        catch(Exception e){
-            throw new RuntimeException("Something went wrong");
-
-        }
-    }
-
+/**
+ * Simple model to describe a postal code currently affected by hub damage.
+ */
+public record DamagedPostalCodes(String postalCode, int populationWithoutPower) {
 }
--- src/HubImpact.java
+++ src/HubImpact.java
@@ -1,92 +1,9 @@
 package ps.csci3901;
-import java.sql.Connection;
-import java.sql.Statement;
+
 import java.util.List;
-import java.sql.ResultSet;
-import java.util.ArrayList;
-import java.sql.*;
-import java.sql.DriverManager;
 
-public class HubImpact extends PowerService  {
-    List damagedhub = null;
-    private Connection h;
-    private Connection rp;
-    private String ResultSet;
-
-    public List <HubImpact>fixOrder ( int limit )throws SQLException{
-        List<HubImpact> list = new ArrayList<> ();
-        try {
-            Statement statement = h.createStatement ( );
-            // Executing the SQL Query
-            String s = "SELECT Postalcode.Postalcodes , Postalcode.population\\r\n" +
-                    "\"FROM DistributionHub\\n\" +\n" +
-                    "\"JOIN hubdamage ON DistributionHub.hubIdentifier = hubdamage.damagedhub\\n\" +\n" +
-                    "\"JOIN Postalcode ON DistributionHub.Postalcodes = Postalcode.Postalcodes\\n\"\n" +
-                    "\"'ORDER BY Postalcode.population DESC Limit\" *limits\" ;ln) ;";
-            ResultSet rs = statement.executeQuery( String.valueOf ( h ) );
-            while (rs.next()) {
-                damagedhub.get ( rs.getInt ( "fixOrder" ) );
-            }
-            // close the sql connection
-            rs.close();
-
-        }
-        catch (Exception e) {
-            System.out.println(e);
-        }
-        return damagedhub;
-    }
-
-    public <HubImpact> List <HubImpact>repairPlan(String startHub, int maxDistance, float maxTime, List<HubImpact> HubImpact, ResultSet ty)throws SQLException {
-        return repairPlan ( startHub, maxDistance, maxTime, HubImpact, ty, ry );
-    }
-
-    public <HubImpact> List <HubImpact>repairPlan (String startHub, int maxDistance, float maxTime, List<HubImpact> HubImpact, ResultSet ty, char[] ry)throws SQLException{
-        List<HubImpact> list = new ArrayList<> ();
-
-        try {
-
-            Statement statement;
-            statement = rp.createStatement ( );
-            // Executing the SQL Query
-            String s=" " +
-                    "";
-
-            s = ResultSet;
-            ty = statement.executeQuery ( String.valueOf ( rp) );
-            while (ty.next()) {
-                HubImpact.get (ty.getInt ( "fixOrder" ) );
-            }
-            // close the sql connection
-            ty.close();
-        } catch (Exception e) {
-            System.out.println(ty);
-        }
-        return HubImpact;
-    }
-    public static void main( String[] args){
-    try{
-        // Connecting to Dal Database
-
-            Class.forName( "com.mysql.cj.jdbc.Driver" );
-            String dbURL="jdbc:mysql://db.cs.dal.ca:3306/jdbcdb";
-            String CSID="riyap";
-            String BANNER_ID="B00930901";
-            connection = DriverManager.getConnection( ("jdbc:mysql://db.cs.dal.ca:3306/csci3901/riyap","dbURL"," CSID","BANNER_ID");
-            final boolean execute;
-             if (statement.execute( "use riyap;" ))
-             {
-                 execute = true;
-             }
-             else {
-                 execute = false;
-             }
-    }
-    catch(Exception e){
-        throw new RuntimeException("Something went wrong");
-    }
-    }
-    public void setRp(Connection rp) {
-        this.rp = rp;
-    }
+/**
+ * Represents one damaged hub and the shortest known route to reach it.
+ */
+public record HubImpact(String hubIdentifier, int distance, List<String> route) {
 }
